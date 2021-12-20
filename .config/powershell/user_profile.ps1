2325e2cadbf0252ea6df5d815a3ab1ff5f39df49
Frank Schroeder <frank@datateam.lu>
2021-12-20 13:43:36 +0100

    First Release of the OMP config files for PowerShell

---
+++ .config/powershell/user_profile.ps1
@@ -0,0 +1,34 @@
+#Load Modules
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module Terminal-Icons
+
+#Config Prompt
+$omp_config = Join-Path $PSScriptRoot ".\config.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+
+#Define Aliases
+Set-Alias -Name vim -Value nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
