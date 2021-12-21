be63fffdcaaade710629609e27e0576aa6b54189
Admin - Frank Schroeder <RJL381_admin@gouv.etat.lu>
2021-12-21 11:48:03 +0100

    Added missing config entry in user_profile.ps1

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,14 +1,9 @@
 #Load Modules
 Import-Module posh-git
 Import-Module oh-my-posh
-Import-Module Terminal-Icons
+Import-Module -Name Terminal-Icons
 
-#Config Prompt
-$omp_config = Join-Path $PSScriptRoot ".\config.omp.json"
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
-
-
-#Define Aliases
+#Load Aliases
 Set-Alias -Name vim -Value nvim
 Set-Alias ll ls
 Set-Alias g git
@@ -16,15 +11,24 @@ Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 
+
+#Set Prompt
+$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
 # PSReadLine
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
 
 # Fzf
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Env
+$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 
 # Utilities
 function which ($command) {
